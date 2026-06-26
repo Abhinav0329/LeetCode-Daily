6e9da310c27eb8b238445fa77db73b932e66db7a
Abhinav0329 <abhinavupadhyay0329@gmail.com>
2026-06-26 12:22:07 +0530

    Time: 0 ms (100%), Space: 13.3 MB (96.99%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -2,9 +2,15 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans(n*2);
-        for(int i=0;i<n;i++){
-            ans[2 * i] = nums[i];
-            ans[2 * i+1] = nums[i+n];
+        int k=2;
+        for(int i=0;i<n*2;i++){
+            if(i%2==0){
+            ans[i]=nums[i/2];
+            }
+            else{
+                ans[i]=nums[n+(i/2)];
+                k--;
+            }
         }
         return ans;
     }
