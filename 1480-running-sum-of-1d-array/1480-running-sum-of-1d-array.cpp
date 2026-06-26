103dda2ca124ee71f8acb3de1ad69c01eb69674f
Abhinav0329 <abhinavupadhyay0329@gmail.com>
2026-06-26 09:46:21 +0530

    Time: 0 ms (100%), Space: 12.6 MB (52.53%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -5,8 +5,8 @@ public:
         vector<int> ans(n);
         int sum = 0;
         for(int i=0;i<n;i++){
-            ans[i]=sum+nums[i];
-            sum = sum+nums[i];
+            ans[i] = sum + nums[i];
+            sum += nums[i];
         }
         return ans;
     }
