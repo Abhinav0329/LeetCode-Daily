6a5a9daef7f8d3e1f3569c0df595f50327dbd32f
Abhinav0329 <abhinavupadhyay0329@gmail.com>
2026-06-27 17:37:50 +0530

    Time: 4 ms (65.45%), Space: 13.5 MB (51.32%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -11,12 +11,13 @@ public:
 };
 /*
 we can use another method:-
+    int k=2;
    for(int i=0;i<n*2;i++){
             if(i%2==0){
             ans[i]=nums[i/2];
             }
             else{
-                ans[i]=nums[n+(i/2)];
+                ans[i]=nums[k+(i/2)];
                 k--;
             }
         }
