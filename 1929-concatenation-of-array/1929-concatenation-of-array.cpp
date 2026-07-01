ca15275d584a0a0fd1ef3e04926645b93e41c521
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-07-01 20:47:20 +0530

    Time: 0 ms (100%), Space: 16.9 MB (60.31%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,9 +1,10 @@
+#include<vector>
 class Solution {
 public:
-    vector<int> getConcatenation(vector<int>& nums) {
+    std::vector<int> getConcatenation(vector<int>& nums) {
     int n = nums.size();
     //Vector Initialization: To make vector'ans' of size double of previous vector'nums'.
-    vector<int> ans(n * 2);
+    std::vector<int> ans(n * 2);
     //to copy half elements in first and rest into second vector 
         for(int i=0;i<n;i++){
             ans[i] = nums[i];//first half
