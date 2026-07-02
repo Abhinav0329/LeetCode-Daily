570af9b4d8ea1bbb4ebf6dcf90eb91cb7bb5e845
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-07-02 20:08:12 +0530

    Time: 0 ms (100%), Space: 13.4 MB (66.45%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -17,3 +17,18 @@ public:
     return evendigit;
     }
 };
+
+/*
+we can use logarithmic value extraction method to ensure O(1) time complexity
+int evencount = 0;
+        for (int num : nums) {
+            // Using log10 to find the number of digits instantly
+            // log10(100) = 2 -> floor(2) + 1 = 3 digits
+            int digitCount = floor(log10(num)) + 1;
+            
+            // Check if the count of digits is even
+            if(digitCount%2==0){
+                evenCount++;
+            }
+        }
+*/
