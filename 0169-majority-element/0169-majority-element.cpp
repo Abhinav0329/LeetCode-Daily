0333f73aaa28caaf37da066aebe8ba65c94abc9e
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-07-04 19:55:30 +0530

    Time: 0 ms (100%), Space: 28.1 MB (89.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,13 +3,14 @@ public:
     int majorityElement(vector<int>& nums) {
         int candidate = 0;
         int count = 0;
-        for (int num : nums) {
+        // we can use for (int num : nums) is called a Range-Based For Loop (or a For-Each loop) instead of dealing with indices, counters, and array boundaries
+        for (int i=0;i<nums.size();i++){
             // Step 1: If the count drops to 0, pick a new candidate
             if (count == 0) {
-                candidate = num;
+                candidate = nums[i];
             }
             // Step 2: Update the weight of the candidate
-            if (num == candidate) {
+            if (nums[i] == candidate) {
                 count++;
             } else {
                 count--;
