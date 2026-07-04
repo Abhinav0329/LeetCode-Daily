c949f0a6668b634a047ed0c9c8e549aa6da04bfe
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-07-04 20:21:46 +0530

    Time: 3 ms (28.29%), Space: 97.3 MB (60.47%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,27 @@
+#include<cmath> // required for INT_MAX
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = INT_MAX;// Start with the highest possible upper bound
+        int max_profit = 0;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<min_price){
+                min_price = prices[i];//// Found a cheaper valley day to buy
+            }
+            else if(prices[i] - min_price > max_profit){
+                max_profit = prices[i] - min_price;
+            }
+        }
+        return max_profit;
+    }
+};
+
+/*
+❌ DO NOT DO THIS (Time Limit Exceeded)
+for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+        max_profit = max(max_profit, prices[j] - prices[i]);
+    }
+}
+While logically sound, this O(N^2) brute force approach will hit a hard wall and crash with a Time Limit Exceeded (TLE) error on LeetCode's massive test cases.I must use the single-pass tracking method
+*/
