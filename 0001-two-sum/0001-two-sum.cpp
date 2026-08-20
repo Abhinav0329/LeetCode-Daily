fdce591307a967c429092d201eb0ba48d217d223
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-08-20 16:28:10 +0530

    Time: 44 ms (16.07%), Space: 14 MB (97.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -11,3 +11,22 @@ public:
         return {};
     }
 };
+
+/*
+Method 2:
+using hash map-
+#include<unordered_map>
+// Map to store: number -> its index
+std::unordered_map<int,int> num_to_index;
+for(int i=0;i<nums.size();i++){
+    int complement = target - nums[i];
+// If the complement exists in our map, we found the pair
+    if(num_to_index.find(complement) != num_to_index.end()){
+        return {num_to_index[compliment],i};
+    }
+    // Store current number and its index
+    num_to_index[nums[i]] = i;
+}
+return {};// Fallback (problem guarantees exactly one solution)
+
+*/
