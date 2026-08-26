8e784041a758a2275f35182c4da8507d05907bbf
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-08-26 22:52:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (65.67%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
+        //Base cases
         if(n <= 1) return n;
         int a = 0;
         int b = 1;
         int next_term = 0;
 
+        // Loop runs up to n (inclusive)
         for(int i=2;i<=n;i++){
             next_term = a + b;
             a = b;
