919f8e0b156b546fca471311b11c750a46b068ab
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-08-29 16:03:12 +0530

    Time: 13 ms (58.1%), Space: 19.1 MB (44.28%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxcount = 0;
+        // Direct Map Array initialized to -1 for all 256 ASCII characters
+        vector<int> lastIndex(256,-1);
+        //string can contain standard ASCII characters (letters, numbers, symbols, spaces).
+        //It serves as a default marker indicating that we have not seen this character yet.
+
+        int l = 0,r = 0,maxlen = 0;
+        int n =static_cast<int>(s.length());
+        while(r < n){
+    // If the character was seen inside or after the current window boundary 'l'
+            if(lastIndex[s[r]] != -1){
+                l = std::max(l,lastIndex[s[r]]+1);// Jump 'l' directly
+            }
+            lastIndex[s[r]] = r;// Update last seen position
+            maxlen = std::max(maxlen,r-l+1);// Calculate current valid window size
+            r++;
+
+        }
+        return maxlen;
+    }
+};
+
+/*
+->runs in O(N²) time complexity
+int maxcount = 0;
         int n = static_cast<int>(s.length());
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
@@ -21,5 +44,4 @@ public:
             }
         }
         return maxcount;
-    }
-};
+*/
