fd87e0204226db5e676837d7d82ed6964300b7a0
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-08-29 13:20:03 +0530

    Time: 0 ms (100%), Space: 11.7 MB (13.65%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = static_cast<int>(code.size());
+        vector<int> arr(n,0);
+        if(k == 0) {
+            return arr;
+        }
+        int l = 1,r = k;
+        if(k < 0){
+            k = abs(k);
+            l = n-k;
+            r = n-1;
+        }
+        int windowsum = 0;
+        for(int i=l;i<=r;i++){
+            windowsum += code[i % n];
+        }
+        for(int i=0;i<n;i++){
+            arr[i] = windowsum;
+            windowsum -= code[l % n];
+            l++;
+            r++;
+            windowsum += code[r % n];
+        }
+        return arr;
+    }
+};
