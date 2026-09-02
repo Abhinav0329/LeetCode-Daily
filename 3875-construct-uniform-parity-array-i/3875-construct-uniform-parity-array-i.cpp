1f9ad71d8af5648985d5dcf1b077fb51513c48ec
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-09-02 11:40:08 +0530

    Time: 0 ms (100%), Space: 31 MB (7.23%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -11,3 +11,11 @@ public:
         return true;
     }
 };
+/*
+nums1 contains distinct integers, if it has at least one odd number, we can make all elements odd. If it has no odd numbers, all elements are already even.
+Parity Rules:
+even−even=even
+odd−odd=even
+even−odd=odd
+odd−even=odd
+*/
