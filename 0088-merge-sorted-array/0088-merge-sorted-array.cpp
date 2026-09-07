2652f45451b30b125294626bd41544798b275ae8
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-09-07 16:18:00 +0530

    Time: 0 ms (100%), Space: 12.3 MB (84.81%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;     // Pointer for the end of valid elements in nums1
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+
+        while(p1>=0 && p2>=0){
+            if(nums1[p1] > nums2[p2]){
+                nums1[p--] = nums1[p1--];
+            }
+            else{
+                nums1[p--] = nums2[p2--];
+            }
+        }
+
+        while(p2 >= 0){
+            nums1[p--] = nums2[p2--];
+        } 
+    }
+};
+
+/*
+int i = m - 1;     // Pointer for the end of valid elements in nums1
         int j = n - 1;     // Pointer for the end of nums2
         int k = m + n - 1; // Pointer for the very end of nums1's total capacity
         
@@ -25,5 +45,4 @@ public:
         }
         
         // No return statement needed since the return type is void!
-    }
-};
+*/
