e400c6665c6f0aac6e647f5370568eae316d280c
Abhinav Upadhyay <abhinavupadhyay0329@gmail.com>
2026-09-24 01:24:51 +0530

    Time: 3 ms (10.17%), Space: 15.2 MB (52.41%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -4,9 +4,9 @@ public:
        sort(nums.begin(),nums.end());
        vector<int> result;
        for(int i=0;i<nums.size();i++){
-        if(nums[i] == target){
+            if(nums[i] == target){
             result.push_back(i);
-        }
+            }
        }
        return result; 
     }
